a3f9905e1a7cf31fdfbaaac0574bae80a5591dd3
gfjardim <gfjardim@gmail.com>
2017-02-02 22:47:48 -0200

    Update Statistics Sender to 2017.02.02
    - Add: Send Dismissed Statistics to Tools

--- source/statistics.sender/StatsSender.php
+++ source/statistics.sender/StatsSender.php
@@ -1,5 +1,6 @@
 <?
 $plugin = "statistics.sender";
+ignore_user_abort(true);
 
 function lsDir($root, $ext = null)
 {
@@ -49,6 +50,26 @@ switch ($_REQUEST['action'])
     }
     break;
 
+  case 'get_dismissed':
+    $reports = array_filter(lsDir("/boot/config/plugins/"), function($v){ return preg_match("#\.sreport\.dismiss$#", $v);});
+    $output = [];
+    if (count($reports))
+    {
+      $rep = &$output["reports"];
+      foreach ($reports as $key => $rfile) {
+        $report = parse_ini_file($rfile, true) ?: [];
+        $mtime = filemtime($rfile);
+        if (array_key_exists("report", $report))
+        {
+          $report["report"]["file"] = $rfile;
+          $report["report"]["select"] = date("M j, Y, G:i:s", $mtime).": ".(preg_replace("#<[^>]*>#i", " - ", $report["report"]["title"]));
+          $rep[$rfile] = $report;
+        }
+      }
+    }
+    echo json_encode($output);
+    break;
+
   case 'send_statistics':
     $file = $_POST["file"];
     echo shell_exec("/usr/local/emhttp/plugins/statistics.sender/scripts/send_statistics ".escapeshellarg($file));
@@ -56,7 +77,10 @@ switch ($_REQUEST['action'])
 
   case 'remove_statistics':
     $file = $_POST["file"];
-    @rename($file,"${file}.dismiss");
+    if (!preg_match("#\.dismiss$#", $file))
+    {
+      @rename($file,"${file}.dismiss");
+    }
     break;
 }
 
