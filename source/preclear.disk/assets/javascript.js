e1fbaaa69e6f773317b397b7b362076c05862576
dlandon <dlandon@familyoflandon.com>
2022-02-16 12:04:38 -0600

    Add copyright notices to all source.

--- source/preclear.disk/assets/javascript.js
+++ source/preclear.disk/assets/javascript.js
@@ -1,3 +1,14 @@
+/* Copyright 2015-2020, Guilherme Jardim
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+
+
 var PreclearURL = '/plugins/'+preclear_plugin+'/Preclear.php'
 var PreclearData = {};
 
--- source/preclear.disk/script/preclear.sh
+++ source/preclear.disk/script/preclear.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# Copyright 2015-2020, Guilherme Jardim
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 2,
+# as published by the Free Software Foundation.
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# */
+
 HEIGHT=15
 WIDTH=60
 
